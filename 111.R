8fe600e9d4b1c8ab265af611cc20bd7e58738d36
a104260016 <43463719+a104260016@users.noreply.github.com>
2018-09-28 16:07:43 +0800

    Add files via upload

---
+++ 111.R
@@ -0,0 +1,73 @@
+my_num1 <- 2.33
+my_num1
+my_num2 <- 2.0
+my_num2
+my_num3 <- 2
+my_num3
+
+my_int1 <- 2L
+my_int1
+my_int2 <- 2.0L
+my_int2
+my_int3 <- 2.33L
+my_int3
+
+TRUE
+FALSE
+T
+F
+True
+true
+
+8 > 7 # 耞 8 琌??? 7
+8 < 7 # 耞 8 琌??? 7
+8 >= 7 # 耞 8 琌???单 7
+8 <= 7 # 耞 8 琌???单 7
+8 == 7 # 耞 8 琌单 7
+8 != 7 # 耞 8 琌ぃ单 7
+7 %in% c(8, 7) # 耞 7 琌??? c(8, 7) ぇい
+
+first_name <- "Tony"
+first_name
+class(first_name)
+
+sys_date <- Sys.Date() # ╰参???戳
+sys_date # 癬ㄓ蛤???
+class(sys_date)
+
+sys_time <- Sys.time() # ╰参丁
+sys_time # 癬ㄓ蛤???
+class(sys_time)
+
+my_weight
+my_height <- 160
+my_weight <- 55
+
+class(2)
+class(2L)
+class(TRUE)
+class("Learning R the easy way")
+class(Sys.Date())
+class(Sys.time())
+
+my_int1 <- 2L
+class(my_int1)
+my_int2 <- 2.0L
+class(my_int2)
+my_int3 <- 2.33L
+class(my_int3)
+class(TRUE)
+class(FALSE)
+class(T)
+class(F)
+class(True)
+class(true)
+first_name <- 'Tony'
+first_name
+class(first_name)
+
+is.numeric(8.7)
+is.numeric("8.7")
+
+is.integer(7L)
+is.integer(7)
