4c636482ad82cf6ab8c1a6549f5284c1a74502cc
a104260016 <43463719+a104260016@users.noreply.github.com>
2018-11-14 23:20:52 +0800

    Add files via upload

---
+++ github題目.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(1,10):
+      for j in range(1,10):
+            print(i, '*', j, '=', i*j, '\t', sep='',end='')
+      print()
+
+
+# In[2]:
+
+
+def main():
+    score = float(input('请输入成绩: '))
+    if score >= 90:
+        grade = 'A'
+    elif score >= 80:
+        grade = 'B'
+    elif score >= 70:
+        grade = 'C'
+    elif score >= 60:
+        grade = 'D'
+    else:
+        grade = 'E'
+    print('对应的等级是:', grade)
+
+if __name__ == '__main__':
+    main()
+
+
+# In[3]:
+
+
+while i < 50:
+ print math.sqrt(i)* 10
+
+
+# In[4]:
+
+
+while i < 50:
+ print math.sqrt(i)* 10
+ sum 10
+
+
+# In[5]:
+
+
+
+    
+    def func(金額、年收益率、經過的月份數):
+ x = a + b + c
+ return x
+
+
+# In[6]:
+
+
+def function_name(輸入, 金額、年收益率、經過的月份數):
+    x = a + b + c
+ return x
+
+
+# In[ ]:
+
+
+
+
